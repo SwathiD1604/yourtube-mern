0c0416751d5d22f70acd3c905a5392073e2c6a62
Swathi D <swathidhanaraj1604@gmail.com>
2026-06-23 22:30:24 +0530

    fix AuthContext TypeScript build issue

--- server/routes/video.js
+++ server/routes/video.js
@@ -1,11 +1,32 @@
 import express from "express";
-import { getallvideo, uploadvideo, downloadvideo, getdownloadedvideos } from "../controllers/video.js";
+import {
+  getallvideo,
+  uploadvideo,
+  downloadvideo,
+  getdownloadedvideos
+} from "../controllers/video.js";
+
 import upload from "../filehelper/filehelper.js";
 
-const routes = express.Router();
+const router = express.Router();
 
-routes.post("/upload", upload.single("file"), uploadvideo);
-routes.get("/getall", getallvideo);
-routes.post("/download/:id", downloadvideo);
-routes.get("/downloaded/:userId", getdownloadedvideos);
-export default routes;
+// Upload video
+router.post("/upload", upload.single("file"), uploadvideo);
+
+// Get all videos (IMPORTANT FIX: safe handler wrapper)
+router.get("/getall", async (req, res) => {
+  try {
+    return await getallvideo(req, res);
+  } catch (error) {
+    console.log("getallvideo error:", error);
+    return res.status(500).json([]);
+  }
+});
+
+// Download video
+router.post("/download/:id", downloadvideo);
+
+// Get downloaded videos
+router.get("/downloaded/:userId", getdownloadedvideos);
+
+export default router;
