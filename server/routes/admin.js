042e6b0b159233664c28ccb247c5c7f4e17d3214
Swathi D <swathidhanaraj1604@gmail.com>
2026-06-24 14:28:27 +0530

    Allow GET request for fix-video-paths endpoint

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,6 +3,8 @@ import { fixVideoPaths } from "../controllers/admin.js";
 
 const router = express.Router();
 
+// Accept both GET and POST for easier access
+router.get("/fix-video-paths", fixVideoPaths);
 router.post("/fix-video-paths", fixVideoPaths);
 
 export default router;
